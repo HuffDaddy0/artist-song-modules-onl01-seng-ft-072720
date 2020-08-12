34cd52b65d015f6890ac37472174c60e61d06d05
Tyler Huffman <ty.huff24@gmail.com>
2020-08-12 18:52:54 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Artist
   extend Memorable
   extend Findable
+  extend Paramable
 
   attr_accessor :name
   attr_reader :songs
@@ -37,7 +38,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
